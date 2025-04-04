f529b8fa14cdb8f2fa08e2cb6fe17342448c09dd
massimocristi1970 <massimo@ticktockloans.com>
2025-04-04 15:54:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ scaler = joblib.load('scaler.pkl')
 
 # Streamlit App
 def main():
-    st.title("Business Finance & Application Analyzer")
+    st.title("Business Finance Application Scorecard")
     requested_loan = st.number_input("Enter the requested loan amount:", min_value=0.0)
     industry = st.selectbox("Select Industry", list(industry_thresholds_config.keys()))
     industry_thresholds = industry_thresholds_config[industry]
