233e9edaea5a423ef551756de4c3fc98042e1945
massimocristi1970 <massimo@ticktockloans.com>
2025-05-06 10:52:04 +0100

    Update plaid_config.py

--- plaid_config.py
+++ plaid_config.py
@@ -14,7 +14,8 @@ COMPANY_ACCESS_TOKENS = dict(sorted({
     "Bound Studios": "access-production-c96f81d2-206f-4e6a-b442-a18d102b3870",
     "Moving Ewe": "access-production-31009602-a97b-42db-bf7c-ca7db185eac1",
     "Sanitaire Ltd": "access-production-9d27e5a4-3f09-4deb-8d7c-58b2ea014578",
-    "Ellevate limited": "access-production-a93de1cc-4c56-4e4f-a92d-6dd000d00d85"
+    "Ellevate limited": "access-production-a93de1cc-4c56-4e4f-a92d-6dd000d00d85",
+    "Boiler Solution Cover UK": "access-production-b8b19025-2c67-4364-b7ee-ac6c236764f1"
 }.items()))
 
 
