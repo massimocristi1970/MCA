eb685129bb6513dd2afcbe31c20e4e737a2bcaa2
massimocristi1970 <massimo@ticktockloans.com>
2025-05-20 13:07:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -455,6 +455,9 @@ penalties = {
     "personal_default_12m": 5,
     "business_ccj": 5,
     "director_ccj": 5,
+    'website_or_social_outdated': 3,
+    'uses_generic_email': 3,
+    'no_online_presence': 5
 }
 
 def calculate_risk(requested_loan, monthly_average_revenue):
