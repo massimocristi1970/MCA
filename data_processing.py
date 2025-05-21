2a49bdb17e578bda092c59f67495540e3c3649c8
massimocristi1970 <massimo@ticktockloans.com>
2025-05-21 18:16:43 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -80,7 +80,7 @@ def map_transaction_category(transaction):
         return "Income"
     if is_credit and re.search(r"(you\s?lend|yl\s?ii|yl\s?ltd|yl\s?limited|yl\s?a\s?limited).*\b(fnd|fund|funding)\b", combined_text):
         return "Loans"
-    if is_credit and re.search(r"\biwoca\b|\bcapify\b|\bfundbox\b|\bgot capital\b", combined_text):
+    if is_credit and re.search(r"\biwoca\b|\bcapify\b|\bfundbox\b|\bgot[\s\-]?capital\b", combined_text):
         return "Loans"
     if is_debit and re.search(r"\biwoca\b", combined_text):
         return "Debt Repayments"
