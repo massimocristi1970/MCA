5eaf0436e10ef6eb2ff44e9eea80b18a1008be1a
Maaz <maaz@ticktockloans.com>
2025-06-10 12:51:53 +0530

    updated model
    Used randomforest for better overall accuracy

--- model_utils.py
+++ model_utils.py
@@ -5,21 +5,26 @@ import numpy as np
 def predict_score(model, metrics, directors_score, sector_risk, scaler, company_age_months):
     # Prepare the input data as a dictionary
     features = {
-        'Directors Score': directors_score,                  
-        'Sector': sector_risk,                             
-        'Total Revenue': metrics["Total Revenue"],         
-        'Total Expenses': metrics["Total Expenses"],       
-        'Net Income': metrics["Net Income"],               
-        'Total Debt Repayments': metrics["Total Debt Repayments"],  
-        'Total Debt': metrics["Total Debt"],               
+        'Directors Score': directors_score,
+        'Total Revenue': metrics["Total Revenue"],  
+        'Total Debt': metrics["Total Debt"], 
         'Debt-to-Income Ratio': metrics["Debt-to-Income Ratio"], 
-        'Expense-to-Revenue Ratio': metrics["Expense-to-Revenue Ratio"],
-        'Operating Margin': metrics["Operating Margin"],     
-        'Debt Service Coverage Ratio': metrics["Debt Service Coverage Ratio"], 
-        'Gross Burn Rate': metrics["Gross Burn Rate"],       
-        'Cash Flow Volatility': metrics["Cash Flow Volatility"], 
-        'Revenue Growth Rate': metrics["Revenue Growth Rate"],  
+        'Operating Margin': metrics["Operating Margin"],  
+        'Debt Service Coverage Ratio': metrics["Debt Service Coverage Ratio"],
+        'Cash Flow Volatility': metrics["Cash Flow Volatility"],
+        'Revenue Growth Rate': metrics["Revenue Growth Rate"],
+        'Average Month-End Balance': metrics["Average Month-End Balance"],
+        'Average Negative Balance Days per Month': metrics["Average Negative Balance Days per Month"],
+        'Number of Bounced Payments': metrics["Number of Bounced Payments"],
         'Company Age (Months)': company_age_months,
+        'Sector_Risk': sector_risk                             
+                
+        # 'Total Expenses': metrics["Total Expenses"],       
+        # 'Net Income': metrics["Net Income"],               
+        # 'Total Debt Repayments': metrics["Total Debt Repayments"],  
+        # 'Expense-to-Revenue Ratio': metrics["Expense-to-Revenue Ratio"],
+        # 'Gross Burn Rate': metrics["Gross Burn Rate"],       
+        
     }
 
     # Convert features to a DataFrame
@@ -33,6 +38,6 @@ def predict_score(model, metrics, directors_score, sector_risk, scaler, company_
     features_scaled = scaler.transform(features_df)
 
     # Predict the probability of repayment (class 1)
-    probability_score = model.predict_proba(features_scaled)[:, 1]
+    probability_score = model.predict_proba(features_scaled)[:, 1] * 100
 
-    return probability_score[0]
+    return round(probability_score[0], 2)
